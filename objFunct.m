fb2887ad5485b327b11400cc56d6b841bff8c790
g-coppola <g.coppola134@gmail>
2025-11-05 15:45:37 +0100

    Prima

--- objFunct.m
+++ objFunct.m
@@ -1,7 +1,8 @@
-function [J] = objFunct(Z,h,Q,R,P,param)
+function [J] = objFunct(Z,h,A,B,Q,R,P,param)
     J = 0;
     n = param.n;
     m = param.m;
+    l = size(A,2);
     
     num_X_vars = n * (h + 1);
     X_vec = Z(1:num_X_vars);
@@ -14,10 +15,17 @@ function [J] = objFunct(Z,h,Q,R,P,param)
     for k = 1:h
         u = U(:,k); 
         x = X(:,k);
-        J = J + x' * Q * x + u' * R * u;
+        c = cell(1,l);
+        for i = 1:l
+            c{i} = (A{i}*x+B{i}*u)'*Q*(A{i}*x+B{i}*u)+ u'*R *u;
+        end
+        J = J + max(c);
     end
 
-    % Costo terminale
-    xf = X(:,h+1); 
-    J = J + xf' * P * xf;
+    % Terminal Cost
+    cf = cell(1,l);
+    for i = 1:l
+        cf{i} = (A{i}*X(:,h)+B*U(:,h))'*P*(A{i}*X(:,h)+B*U(:,h));
+    end
+    J = J + max(cf);
 end
